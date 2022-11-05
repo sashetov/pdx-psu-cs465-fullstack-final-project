2752d0293dabdcba9b2b5b1f65eefebb20d12e5f
gleason9113 <68160110+gleason9113@users.noreply.github.com>
2022-11-05 12:16:19 -0700

    Built outline in React

---
+++ src/components/Board.js
@@ -0,0 +1,43 @@
+function Board() {
+  return (
+    <div className="container-sm w-50">
+        <div className="row">
+          <div className="col cell">
+            X
+          </div>
+          <div className="col cell">
+            O
+          </div>
+          <div className="col cell">
+            X
+          </div>
+        </div>
+        <div className="row">
+        <div className="col cell">
+            O
+          </div>
+          <div className="col cell">
+            X
+          </div>
+          <div className="col cell">
+            O
+          </div>
+        </div>
+        <div className="row">
+        <div className="col cell">
+            X
+          </div>
+          <div className="col cell">
+            O
+          </div>
+          <div className="col cell">
+            X
+          </div>
+        </div>
+        
+    </div>
+  );
+  
+}
+
+export default Board;
