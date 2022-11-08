43b0dbcfb2fe05c10c2a462c7b7d3a416720b813
Gleason <gleasoa1@nala.roche.com>
2022-11-08 14:26:08 -0800

    Test commit

--- src/components/Board.js
+++ src/components/Board.js
@@ -25,6 +25,35 @@ const Board = ({ socket }) => {
   const reference = useRef(null);
 
   //Functions
+  const checkWinner = () => {
+    const board = [...boardState];
+    for (let i = 0; i < 9; i += 3) {
+      if (board[i] === board[i+1] === board[i+2]) {
+        winner = board[i];
+      }
+    }
+    for(let i = 0; i < 3; i++) {
+      if (board[i] === board[i+3] === board[i+6]) {
+        winner = board[i];
+      }
+    }
+    if(board[0] === board[4] === board[8] || board[2] === board[4] === board[6]) {
+      winner = board[4];
+    }
+    return winner;
+  }
+
+  const checkTie = () => {
+    const board = [...boardState];
+    let counter = 0;
+    board.forEach((space) => {
+      if (space !== '') {
+        counter = counter + 1;
+      }
+    });
+    return counter === 9;
+  }
+
   const move = (event, index) => {
     const newBoard = [...boardState];
     const curr = turn === 0 ? 'X' : 'O';
@@ -36,106 +65,16 @@ const Board = ({ socket }) => {
     setBoard(newBoard);
     console.log(newBoard);
     // check for winner
-    if (index === 0) {
-      if (
-        (newBoard[1] === curr && newBoard[2] === curr) ||
-        (newBoard[3] === curr && newBoard[6] === curr) ||
-        (newBoard[4] === curr && newBoard[8] === curr)
-      ) {
-        winner = curr;
-      }
-    } else if (index === 1) {
-      if (
-        (newBoard[0] === curr && newBoard[2] === curr) ||
-        (newBoard[4] === curr && newBoard[7] === curr)
-      ) {
-        winner = curr;
-      }
-    } else if (index === 2) {
-      if (
-        (newBoard[0] === curr && newBoard[1] === curr) ||
-        (newBoard[5] === curr && newBoard[8] === curr) ||
-        (newBoard[4] === curr && newBoard[6] === curr)
-      ) {
-        winner = curr;
-      }
-    } else if (index === 3) {
-      if (
-        (newBoard[0] === curr && newBoard[6] === curr) ||
-        (newBoard[4] === curr && newBoard[5] === curr)
-      ) {
-        winner = curr;
-      }
-    } else if (index === 4) {
-      if (
-        (newBoard[1] === curr && newBoard[7] === curr) ||
-        (newBoard[3] === curr && newBoard[5] === curr) ||
-        (newBoard[0] === curr && newBoard[8] === curr) ||
-        (newBoard[2] === curr && newBoard[6] === curr)
-      ) {
-        winner = curr;
-      }
-    } else if (index === 5) {
-      if (
-        (newBoard[2] === curr && newBoard[8] === curr) ||
-        (newBoard[3] === curr && newBoard[4] === curr)
-      ) {
-        winner = curr;
-      }
-    } else if (index === 6) {
-      if (
-        (newBoard[0] === curr && newBoard[3] === curr) ||
-        (newBoard[7] === curr && newBoard[8] === curr) ||
-        (newBoard[2] === curr && newBoard[4] === curr)
-      ) {
-        winner = curr;
-      }
-    } else if (index === 7) {
-      if (
-        (newBoard[1] === curr && newBoard[4] === curr) ||
-        (newBoard[6] === curr && newBoard[8] === curr)
-      ) {
-        winner = curr;
-      }
-    } else if (index === 8) {
-      if (
-        (newBoard[2] === curr && newBoard[5] === curr) ||
-        (newBoard[0] === curr && newBoard[4] === curr) ||
-        (newBoard[6] === curr && newBoard[7] === curr)
-      ) {
-        winner = curr;
-      }
-    } else {
-      console.log('Weird error happened. How did you get here?');
-    }
-
-    if (winner === '') {
-      let counter = 0;
-      // check if board is full
-      newBoard.forEach((space) => {
-        if (space !== '') {
-          counter = counter + 1;
-        }
-      });
-
-      // board is full
-      if (counter === 9) {
-        console.log(`Game over :( No one won.`);
-        // end game here
-      }
-      // board is not full
-      else {
-        // continue game
-        socket.emit('move', { boardState: newBoard });
-        setTurn(turn === 0 ? 1 : 0);
-      }
-    } else {
-      console.log(`Player ${curr} won!`);
-      // end game here
-    }
-
+   winner = checkWinner();
+   if (winner !== '') {
+    console.log(`${winner} won!`);
+   } else if (checkTie() === true) {
+    console.log(`Game over :( No one won.`);
+   } else { 
     socket.emit('move', { boardState: newBoard });
     setTurn(turn === 0 ? 1 : 0);
+   }
+
   };
 
   return (
@@ -250,3 +189,99 @@ const Board = ({ socket }) => {
 };
 
 export default Board;
+
+/*
+ if (index === 0) {
+      if (
+        (newBoard[1] === curr && newBoard[2] === curr) ||
+        (newBoard[3] === curr && newBoard[6] === curr) ||
+        (newBoard[4] === curr && newBoard[8] === curr)
+      ) {
+        winner = curr;
+      }
+    } else if (index === 1) {
+      if (
+        (newBoard[0] === curr && newBoard[2] === curr) ||
+        (newBoard[4] === curr && newBoard[7] === curr)
+      ) {
+        winner = curr;
+      }
+    } else if (index === 2) {
+      if (
+        (newBoard[0] === curr && newBoard[1] === curr) ||
+        (newBoard[5] === curr && newBoard[8] === curr) ||
+        (newBoard[4] === curr && newBoard[6] === curr)
+      ) {
+        winner = curr;
+      }
+    } else if (index === 3) {
+      if (
+        (newBoard[0] === curr && newBoard[6] === curr) ||
+        (newBoard[4] === curr && newBoard[5] === curr)
+      ) {
+        winner = curr;
+      }
+    } else if (index === 4) {
+      if (
+        (newBoard[1] === curr && newBoard[7] === curr) ||
+        (newBoard[3] === curr && newBoard[5] === curr) ||
+        (newBoard[0] === curr && newBoard[8] === curr) ||
+        (newBoard[2] === curr && newBoard[6] === curr)
+      ) {
+        winner = curr;
+      }
+    } else if (index === 5) {
+      if (
+        (newBoard[2] === curr && newBoard[8] === curr) ||
+        (newBoard[3] === curr && newBoard[4] === curr)
+      ) {
+        winner = curr;
+      }
+    } else if (index === 6) {
+      if (
+        (newBoard[0] === curr && newBoard[3] === curr) ||
+        (newBoard[7] === curr && newBoard[8] === curr) ||
+        (newBoard[2] === curr && newBoard[4] === curr)
+      ) {
+        winner = curr;
+      }
+    } else if (index === 7) {
+      if (
+        (newBoard[1] === curr && newBoard[4] === curr) ||
+        (newBoard[6] === curr && newBoard[8] === curr)
+      ) {
+        winner = curr;
+      }
+    } else if (index === 8) {
+      if (
+        (newBoard[2] === curr && newBoard[5] === curr) ||
+        (newBoard[0] === curr && newBoard[4] === curr) ||
+        (newBoard[6] === curr && newBoard[7] === curr)
+      ) {
+        winner = curr;
+      }
+    } else {
+      console.log('Weird error happened. How did you get here?');
+    }
+
+    if (winner === '') {
+      let counter = 0;
+      // check if board is full
+      newBoard.forEach((space) => {
+        if (space !== '') {
+          counter = counter + 1;
+        }
+      });
+
+      // board is full
+      if (counter === 9) {
+        console.log(`Game over :( No one won.`);
+        // end game here
+      }
+      // board is not full
+      else {
+        // continue game
+        socket.emit('move', { boardState: newBoard });
+        setTurn(turn === 0 ? 1 : 0);
+      }
+      */
