f4db5609b61046b7016c199dc6bceb2d252be7f7
C3ra906 <95190216+C3ra906@users.noreply.github.com>
2022-11-12 17:21:59 -0800

    starting a new js for splash page recommit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import Banner from './components/Banner';
-import Board  from './components/Board';
+import Board from './components/Board';
+import Splash from './components/Splash';
+
 function App() {
   const [socket, setSocket] = useState(null);
 
@@ -11,10 +13,11 @@ function App() {
     return () => newSocket.close();
   }, [setSocket]);
 
+  // TODO add connection to Splash page when user first opens the domain
   return (
     <div>
-    <Banner />
-    { socket ? ( <Board  socket={socket} /> ) : ( <div>Not Connected</div>) }
+      <Banner />
+      {socket ? <Board socket={socket} /> : <div>Not Connected</div>}
     </div>
   );
 }
---
+++ src/components/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Splash() {
+  return (
+    <div className="container mx-auto text-center">
+      <h1 className="display-1">Welcome to Tic-Tac-Toe</h1>
+      <form></form>
+    </div>
+  );
+}
+
+export default Splash;
