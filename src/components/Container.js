5e82ab29412a2ebb78be8124394e060ec39581db
Gleason <gleasoa1@nala.roche.com>
2022-11-15 09:03:23 -0800

    Switching current work to different machine

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,83 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import Banner from './Banner';
 import Board from './Board';
+import io from 'socket.io-client';
 import Splash from './Splash';
 import { socket } from './socket';
+
+function App() {
+  const [socket, setSocket] = useState(null);
+  const [first, setFirst] = useState(true);
  
-//
-export default class Container extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      socket: null,
-      gameId: null,
-      gameData: null,
-      connected: false,
-      playerName: null,
-      joined:  false
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
-  }
+  useEffect(() => {
+    const newSocket = io.connect(window.location.origin);
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [setSocket]);
 
-  componentDidMount() {
-    socket.on("connected", data => {
-      console.log("Connected:  " + socket.connected);
-      this.setState({ connected: true });
-      console.log(data);
-    });
-  }
-
-  handleChange (event) {
-    event.preventDefault();
-   // this.setState({playerName:"Test"});
-    console.log(this.state);
-  }
-  handleSubmit(event) {
-    event.preventDefault();
-    const { value } = event.target;
-    this.setState({playerName:value});
+  function handleSubmit(event) {
     console.log(event.target.name);
-    console.log("Is this firing?"); 
+    setFirst(event.target.name);
+    console.log("First: " + first);
   }
 
-  render() {
-    return (
-      <div>
-        <Banner />
-        {this.state.connected ? 
-            <Board socket={socket} />
-          : <div>
-              <form
-                id="player"
-                class="form w-50 mx-auto mt-5 p-3"
-                method="get"
-                onSubmit={this.handleSubmit}>
-                <div class="form-group mx-auto my-2">
-                  <label class="py-2" for="name">
-                    Please enter player name:
-                  </label>
-                  <input type="text" class="form-control" onChange={this.handleChange} id="name" />
-                </div>
-                <div class="row form-group mx-auto mb-2">
-                  <button
-                    class="col btn btn-primary my-2 px-5"
-                    type="submit"
-                    name="submit"
-                    value="Play"
-                  />
-                  <button
-                    class="col btn btn-secondary mx-1 my-2 px-5"
-                    type="reset"
-                    name="reset"
-                    value="Reset"
-                  />
-                </div>
-              </form>
-            </div>
-        }  
+  const splash = (
+    <form
+      id="player"
+      class="form w-50 mx-auto mt-5 p-3"
+      method="get"
+      onSubmit={handleSubmit}
+    >
+      <div class="form-group mx-auto my-2">
+        <label class="py-2" for="name">
+          Please enter player name:
+        </label>
+        <input type="text" class="form-control" id="name" />
       </div>
+      <div class="row form-group mx-auto mb-2">
+        <input
+          class="col btn btn-primary my-2 px-5"
+          type="submit"
+          name="submit"
+          value="Play"
+        />
+        <input
+          class="col btn btn-secondary mx-1 my-2 px-5"
+          type="reset"
+          name="reset"
+          value="Reset"
+        />
+      </div>
+    </form>
+  );
+
+  return (
+    <div>
+      <Banner />
+      {first ? (
+        splash
+      ) : socket ? (
+        <Board socket={socket} />
+      ) : (
+        <div>Not Connected</div>
+      )}
+    </div>
   );
 }
-}
+//
+export default App;
