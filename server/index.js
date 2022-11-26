2bc026dfeaad7ba9e723c3b1cb6b1d4e011ef6fd
gleason9113 <68160110+gleason9113@users.noreply.github.com>
2022-11-26 15:27:36 -0800

    Fixed merge conflicts, hopefully.

--- server/index.js
+++ server/index.js
@@ -3,21 +3,17 @@ const http = require('http'),
   app = express(),
   socketIo = require('socket.io');
 const fs = require('fs');
-
 const server = http.Server(app).listen(8080);
 const io = socketIo(server);
 const sockets = {};
 let players = {};
 let games = [];
-
 app.use(express.static(__dirname + '/../build/'));
 app.use(express.static(__dirname + '/../node_modules/'));
-
 app.get('/', (req, res) => {
   const stream = fs.createReadStream(__dirname + '/../build/index.html');
   stream.pipe(res);
 });
-
 app.get('/join', (req, res) => {
   let params = Object.keys(req.query);
   if (params.indexOf('playerName') === -1) {
@@ -30,17 +26,11 @@ app.get('/join', (req, res) => {
       .status(400)
       .send({ status: 'error', message: 'missing socket id parameter' }); // Bad request
   }
-
   let socket_id = req.query.socket_id;
   let playerName = req.query.playerName;
-  console.log('players:', players);
-  console.log('socket_id:', socket_id);
   let player = players[socket_id];
-  console.log('player:', player);
   let gameId = player['gameId'];
-
   if (gameId && games[gameId].winner == null) {
-    console.log(`rejoined game ${gameId}`);
     res.json(players[socket_id]); //rejoin previous unfinished game
   } else {
     // create new game since old one is finished or never started
@@ -54,21 +44,28 @@ app.get('/join', (req, res) => {
         players[socket_id]['symbol'] = 'O'; // player 2 is always 0
         gameId = i;
         players[socket_id]['gameId'] = gameId; // set gameId so client knows its ok to proceed
-        console.log('found game and joined');
-        let data = {
+        let player1_socket = sockets[game['player1']];
+        let player2_socket = sockets[game['player2']];
+        let data1 = {
           status: 'ok',
           msg: 'you have an opponent',
           data: {
-            isYourTurn: false,
+            // first player goes first
+            isYourTurn: true,
+            opponentName: players[player2_socket.id].playerName
           },
         };
-        let player1_socket = sockets[game['player1']];
-        let player2_socket = sockets[game['player2']];
-        data.data.isYourTurn = true;
-        player1_socket.emit('opponentAvailable', data);
-        data.data.isYourTurn = false;
-        player2_socket.emit('opponentAvailable', data);
-        console.log('found game and joined');
+        let data2 = {
+          status: 'ok',
+          msg: 'you have an opponent',
+          data: {
+            // second player does not go first
+            isYourTurn: false,
+            opponentName: players[player1_socket.id].playerName
+          },
+        };
+        player1_socket.emit('opponentAvailable', data1);
+        player2_socket.emit('opponentAvailable', data2);
       }
       if (gameId !== null) break;
     }
@@ -84,10 +81,7 @@ app.get('/join', (req, res) => {
       gameId = games.length - 1;
       players[socket_id]['symbol'] = 'X'; // player 1 is always X
       players[socket_id]['gameId'] = gameId; // set gameId so client knows its ok to proceed
-      console.log('started new game');
     }
-    console.log('games: ', games);
-    console.log('players: ', players);
     res.json(players[socket_id]);
   }
 });
@@ -111,7 +105,6 @@ let checkBoardForWinner = (gameId) => {
   [ O ]
   [   ]
   etc
-  
   Returns status code for winner of a particular gameId game:
     -1 means error because game doesn't have both players yet
     0 means player 1 won
@@ -128,7 +121,6 @@ let checkBoardForWinner = (gameId) => {
     O: null,
   };
   let winner = null; // still in play
-  console.log(game);
   if (players[game['player1']].symbol === 'X') {
     playerMap['X'] = 0;
     playerMap['O'] = 1;
@@ -136,12 +128,9 @@ let checkBoardForWinner = (gameId) => {
     playerMap['O'] = 0;
     playerMap['X'] = 1;
   }
-
   let symbols = ['X', 'O'];
   for (let i = 0; i < symbols.length; i += 1) {
     let s = symbols[i];
-    // 0 1 2 3 4 5 6 7 8
-    // x o x o o x _ _ x 
     if (
       (state[0] === s && state[1] === s && state[2] === s) || //check rows
       (state[3] === s && state[4] === s && state[5] === s) || //check rows
@@ -152,12 +141,10 @@ let checkBoardForWinner = (gameId) => {
       (state[0] === s && state[4] === s && state[8] === s) || //check diagonals
       (state[2] === s && state[4] === s && state[6] === s) //check diagonals
     ) {
-      console.log('got a winner!', s, playerMap[s]);
       winner = playerMap[s];
       break;
     }
   }
-
   if (
     winner === null &&
     state[0] !== '' &&
@@ -171,7 +158,6 @@ let checkBoardForWinner = (gameId) => {
     state[8] !== ''
   )
     winner = 2; // draw
-
   return winner;
 };
 
@@ -189,10 +175,7 @@ io.sockets.on('connection', (socket) => {
     symbol: null,
     gameId: null,
   };
-
   socket.on('move', (data) => {
-    console.log('id:', id);
-    console.log('data:', data);
     data.socket_id = id;
     if (players[id].gameId === null) {
       let data = {
@@ -202,14 +185,10 @@ io.sockets.on('connection', (socket) => {
         data: null,
       };
       socket.emit('move_done', data);
-      console.log('move_done', data);
     } else {
       let gameId = players[id].gameId,
         game = games[gameId],
         moveId = parseInt(data.move_id);
-      console.log('gameId:', gameId);
-      console.log('game:', game);
-      console.log('moveId:', moveId);
       if (!Number.isInteger(moveId)) {
         let data = {
           status: 'error',
@@ -217,7 +196,6 @@ io.sockets.on('connection', (socket) => {
           msg: 'you did not provide a valid move id, you need to provide data that looks like {move_id: move_id}, where move_id is an interger that corresponds to an index in the board array',
           data: null,
         };
-        console.log('move_done', data);
         socket.emit('move_done', data);
       } else if (game['winner'] === -1) {
         // error: game not fully initiallized yet
@@ -228,7 +206,6 @@ io.sockets.on('connection', (socket) => {
           data: null,
         };
         socket.emit('move_done', data);
-        console.log('move_done', data);
       } else if (
         game['state'][moveId] !== undefined &&
         game['state'][moveId] !== ''
@@ -240,7 +217,6 @@ io.sockets.on('connection', (socket) => {
           msg: 'player attempting to move to a slot in the game that already has a symbol in it',
           data: null,
         };
-        console.log('move_done', data);
         socket.emit('move_done', data);
       } else {
         let currPlayer = null;
@@ -262,7 +238,6 @@ io.sockets.on('connection', (socket) => {
             data: null,
           };
           socket.emit('move_done', data);
-          console.log('move_done:', data);
         } else {
           game['state'][moveId] = players[id]['symbol']; // do the move
           game['nextPlayer'] = otherPlayer; // switch next player turn
@@ -283,8 +258,6 @@ io.sockets.on('connection', (socket) => {
           let player2_socket = sockets[game['player2']];
           player1_socket.emit('move_done', data);
           player2_socket.emit('move_done', data);
-          console.log('move_done', data);
-
           if (winner === 0 || winner === 1 || winner === 2) {
             // game is over, can delete game
             delete games[gameId];
@@ -293,11 +266,10 @@ io.sockets.on('connection', (socket) => {
       }
     }
   });
-
   socket.on('chat', (data) => {
     data.socket_id = id;
     let gameId = players[id].gameId,
-        game = games[gameId];
+      game = games[gameId];
     if (players[id].gameId === null) {
       let data = {
         status: 'error',
@@ -306,7 +278,6 @@ io.sockets.on('connection', (socket) => {
         data: null,
       };
       socket.emit('chat_done', data);
-      console.log('chat_done', data);
     } else if (game['winner'] === -1) {
       // error: game not fully initiallized yet
       let data = {
@@ -316,8 +287,7 @@ io.sockets.on('connection', (socket) => {
         data: null,
       };
       socket.emit('chat_done', data);
-      console.log('chat_done', data);
-    } else if ( Object.keys(data).indexOf("message") === -1){
+    } else if (Object.keys(data).indexOf('message') === -1) {
       let data = {
         status: 'error',
         msg: 'chat message not provided, you need to provide it in the data for the socket under the key "message"',
@@ -325,43 +295,35 @@ io.sockets.on('connection', (socket) => {
         data: null,
       };
       socket.emit('chat_done', data);
-      console.log('chat_done', data);
     } else {
       let otherPlayerSocket = null;
       let player1_socket = sockets[game['player1']];
       let player2_socket = sockets[game['player2']];
-      if(player1_socket.id === id){
+      if (player1_socket.id === id) {
         otherPlayerSocket = player2_socket;
-      }
-      else {
+      } else {
         otherPlayerSocket = player1_socket;
       }
       let playerName = players[id].playerName;
       let otherPlayerName = players[otherPlayerSocket.id].playerName;
-      console.log('gameId:', gameId);
-      console.log('game:', game);
-      console.log('playerName:', playerName);
-      console.log('otherPlayerName:', otherPlayerName);
       let dataPlayer = {
         status: 'ok',
-        msg: "sent message successfully to player",
+        msg: 'sent message successfully to player',
         data: {
           message: data.message,
           from: `${playerName}`,
-          to: `${otherPlayerName}`
-        }
-      }
-      let dataOther = {
-        status: 'ok',
-        "msg": "you have a message",
-        data: {
-          message: data.message,
-          from: `${playerName}`,
-          to: `${otherPlayerName}`
+          to: `${otherPlayerName}`,
         },
       };
-      console.log('dataPlayer:', dataPlayer);
-      console.log('dataOther:', dataOther);
+      let dataOther = {
+        status: 'ok',
+        msg: 'you have a message',
+        data: {
+          message: data.message,
+          from: `${playerName}`,
+          to: `${otherPlayerName}`,
+        },
+      };
       socket.emit('chat_done', dataPlayer);
       otherPlayerSocket.emit('chat_done', dataOther);
     }
@@ -372,3 +334,8 @@ io.sockets.on('connection', (socket) => {
     socket.emit('client_disconnect', id);
   });
 });
+let closeServer = () => {
+  server.close();
+  io.close();
+};
+module.exports = {server,closeServer}
