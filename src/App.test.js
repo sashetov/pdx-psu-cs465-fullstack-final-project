fcc989b5ee9fd68e07c523dfb381d2103221aef0
gleason9113 <68160110+gleason9113@users.noreply.github.com>
2022-11-25 17:47:06 -0800

    Debugged a problem with my versions, then fixed the tests.

--- src/App.test.js
+++ src/App.test.js
@@ -7,11 +7,10 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event'
 import { cleanup, render, screen, within, fireEvent } from '@testing-library/react';
-import { shallow } from 'enzyme';
 import Banner from './components/Banner';
 import Board from './components/Board';
 import App from './App';
-import { EnzymeAdapter } from 'enzyme';
+import MockedSocket from 'socket.io-mock';
 
 afterEach(cleanup);
 
@@ -20,6 +19,7 @@ describe("Banner Tests", () => {
   test("Page contains an <h1> element.", () => {
     render(<Banner />);
     const banner = screen.getByRole('heading')
+    expect(banner).toBeDefined();
   })
 
   test("Page contains the correct text.", () => {
@@ -31,5 +31,23 @@ describe("Banner Tests", () => {
 })
 
 describe("Board Tests", () => {
-  
+  test("Board matches snapshot", () => {
+    let socket = new MockedSocket();
+    const view = render(<Board socket={socket} />);
+    expect(view).toMatchSnapshot();
+  })
+
+  test("Calling move function triggers a socket emit event", () => {
+    let socket = new MockedSocket();
+    const view = render(<Board socket={socket} />);
+
+  })
+})
+
+describe("Container Tests", () => {
+
+})
+
+describe("Chat Tests", () => {
+
 })
